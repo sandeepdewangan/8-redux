6326cbd9d79a971b7e94b2422b39768be0b96893
Sandeep <sandeepdewangan7@gmail.com>
2025-02-10 23:55:55 +0530

    Combined Reducers and Actions

--- src/store.js
+++ src/store.js
@@ -1,12 +1,18 @@
-import { legacy_createStore as createStore } from "redux";
+import { combineReducers, legacy_createStore as createStore } from "redux";
 
-const initialState = {
+const initialAccountState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
 
-function reducer(state = initialState, action) {
+const initialCustomerState = {
+  fullName: "",
+  adhaar: "",
+  createdAt: "",
+};
+// Store
+function accountReducer(state = initialAccountState, action) {
   switch (action.type) {
     case "account/deposit":
       return { ...state, balance: state.balance + action.payload };
@@ -17,6 +23,30 @@ function reducer(state = initialState, action) {
   }
 }
 
+function customerReducer(state = initialCustomerState, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        adhaar: action.payload.adhaar,
+        createdAt: action.payload.createdAt,
+      };
+    case "account/updateName":
+      return { ...state, fullName: action.payload.fullName };
+
+    default:
+      return state;
+  }
+}
+// Reducers
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+const store = createStore(rootReducer);
+
+// Actions
 function deposit(amount) {
   return { type: "account/deposit", payload: amount };
 }
@@ -24,11 +54,18 @@ function deposit(amount) {
 function withdraw(amount) {
   return { type: "account/withdraw", payload: amount };
 }
+function createCustomer(fullName, adhaar, createdAt) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName: fullName, adhaar: adhaar, createdAt: createdAt },
+  };
+}
 
-const store = createStore(reducer);
-// store.dispatch({ type: "account/deposit", payload: 100 });
-// store.dispatch({ type: "account/withdraw", payload: 50 });
 store.dispatch(deposit(100));
 store.dispatch(withdraw(50));
+
+store.dispatch(
+  createCustomer("Sandeep Dewangan", "123456789", new Date().toISOString())
+);
 
 console.log(store.getState());
